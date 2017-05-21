1cf98f88142c9c6af789c8ab431daf4d896e7f5d
jaroen007 <jaroeneefting@hotmail.com>
2017-05-21 12:13:14 +0200

    oke

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,33 @@
-setTimeout(function() {   
-   $('.loadingScreen').fadeOut('slow', function() {
-		$('.potOverview > .text').removeClass('hide');
-		$('.potOverview > .text').css('width',$(window).width()); //dynamicly detect device width to always center text
-		$('.potOverview').fadeIn('slow');
-	});
-}, 2000);
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
 
-$('.potOverview > .text').append('now you can do shit');
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+	
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+       console.log('Received Event: ' + id);
+		
+		setTimeout(function() {   
+		   $('.loadingScreen').fadeOut('slow', function() {
+				$('.potOverview > .text').removeClass('hide');
+				$('.potOverview > .text').css('width',$(window).width()); //dynamicly detect device width to always center text
+				$('.potOverview').fadeIn('slow');
+			});
+		}, 2000);
+ 
+		$('.potOverview > .text').append('now you can do shit');
+    }
+};
