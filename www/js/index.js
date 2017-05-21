85bfc1e2a75cce787c3940ed746e45ecd4398dae
jaroen007 <jaroeneefting@hotmail.com>
2017-05-21 04:25:01 +0200

    now for real

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,9 @@
 setTimeout(function() {   
    $('.loadingScreen').fadeOut('slow', function() {
 		$('.potOverview > .text').removeClass('hide');
+		$('.potOverview > .text').css('width',$(window).width()); //dynamicly detect device width to always center text
 		$('.potOverview').fadeIn('slow');
 	});
-}, 5000);
+}, 2000);
+
+$('.potOverview > .text').append('now you can do shit');
